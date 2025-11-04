0416ad9829738fbf67e29da419b24027cb5b4ea7
GooseOb <gooose.ob@gmail.com>
2025-11-04 02:06:16 +0100

    use builtin pad methods for paddings

--- src/formats/lib/pad.js
+++ src/formats/lib/pad.js
@@ -1,10 +1,3 @@
-export const left = (str, length) =>
-  str.length < length ? " ".repeat(length - str.length) + str : str;
-export const right = (str, length) =>
-  str.length < length ? str + " ".repeat(length - str.length) : str;
-
-export const LENGTH = 16;
-
-export const column = (str) => left(str, LENGTH);
-export const month = (str) => right(str, LENGTH / 2);
-export const amount = (floatNum) => left(floatNum.toFixed(2), LENGTH);
+export const month = (str) => str.padEnd(7, " ");
+export const column = (str) => str.padStart(16, " ");
+export const amount = (floatNum) => column(floatNum.toFixed(2));
